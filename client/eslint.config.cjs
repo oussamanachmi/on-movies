4d8abbd3a50db30463b1fc5a6dc35fcc370ca050
OUSSAMA_NACHMI\nachm <nachmi.oussema@gmail.com>
2025-07-02 15:07:28 +0200

    Edit eslint

---
+++ client/eslint.config.cjs
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+      files: ['*.ts'],
+      extends: [
+        'plugin:@angular-eslint/recommended'
+      ],
+      rules: {}
+    },
+    {
+      files: ['*.html'],
+      extends: [
+        'plugin:@angular-eslint/template/recommended'
+      ],
+      rules: {}
+    },
+  ];
