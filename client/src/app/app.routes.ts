f385079687f54dd6742f47734cdbfe5c29d972dd
OUSSAMA_NACHMI\nachm <nachmi.oussema@gmail.com>
2025-07-02 13:11:40 +0200

    Add home, navbar and footer component + Install bootstrap

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    // { path: '**', redirectTo: '404' },
+
+    { path: '', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
     {
         path: 'movies',
         loadComponent: () => import('./movies-list/movies-list.component').then(m => m.MoviesListComponent)
-    }
+    },
+    // { path: '404', loadComponent: () => import('./core/components/not-found/not-found.component').then(m => m.NotFoundComponent) },
+    { path: '**', redirectTo: '404' },
 ];
