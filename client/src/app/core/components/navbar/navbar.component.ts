63cc8bbf88f19c8ed2f2d5ece43eee344b80a3ba
OUSSAMA_NACHMI\nachm <nachmi.oussema@gmail.com>
2025-07-02 13:13:26 +0200

    Add routerLink

--- client/src/app/core/components/navbar/navbar.component.ts
+++ client/src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  standalone: true
 })
 export class NavbarComponent {
 
