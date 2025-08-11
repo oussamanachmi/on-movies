bff6e611e95d5be0806bdaee6dc2f1234db16df9
Oussama Nachmi <nachmi.oussema@gmail.com>
2025-08-11 14:10:16 +0200

    Comment movies details route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { movieDetailsComponent } from './features/movie-details/movie-details.component';
+import { RenderMode, ServerRoute } from '@angular/ssr';
+import { Movie } from './core/models/movies.model';
 
 export const routes: Routes = [
 
@@ -7,11 +10,28 @@ export const routes: Routes = [
         path: 'movies',
         loadComponent: () => import('./features/movies-list/movies-list.component').then(m => m.MoviesListComponent)
     },
-    {
-        path: 'movies/:id',
-        loadComponent: () => import('./features/movie-details/movie-details.component').then(m => m.movieDetailsComponent),
-        data: { renderMode: 'server' } // No prerender
-    },
+    // {
+    //     path: 'movies/:id',
+    //     // loadComponent: () => import('./features/movie-details/movie-details.component').then(m => m.movieDetailsComponent),
+    //     component: movieDetailsComponent,
+    //     data: { renderMode: 'server' }  // ou 'client', pour désactiver prerendering
+
+    // },
     // { path: '404', loadComponent: () => import('./core/components/not-found/not-found.component').then(m => m.NotFoundComponent) },
     // { path: '**', redirectTo: '404' },
+];
+
+export const serverRoutes: ServerRoute[] = [
+    {
+        path: 'movies/:id',
+        renderMode: RenderMode.Prerender,
+        async getPrerenderParams() {
+            const movies = await fetch('https://on-movies.onrender.com/movies').then(res => res.json());
+            return movies.map((movie: Movie) => ({ id: movie._id }));
+        },
+    },
+    {
+        path: '**',
+        renderMode: RenderMode.Prerender
+    }
 ];
