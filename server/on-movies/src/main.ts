e6131b417b2ad96cf73fb7f22e598e2e1135f56e
Oussama Nachmi <nachmi.oussema@gmail.com>
2025-08-11 12:02:38 +0200

    Edit port

--- server/on-movies/src/main.ts
+++ server/on-movies/src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for all routes
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
